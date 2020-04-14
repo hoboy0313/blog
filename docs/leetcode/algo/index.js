6fc67baf1c4ec436f7407b19d12bc88491125cd8
liujinghan <jinghan.liu@yunjiaplus.com>
2020-04-14 15:52:44 +0800

    计算机

--- docs/leetcode/algo/index.js
+++ docs/leetcode/algo/index.js
@@ -1,32 +1,8 @@
-
-
-
 /**
- * @param {string} s
+ * @param {number} K
+ * @param {number} N
  * @return {number}
  */
-var lengthOfLongestSubstring = function(s) {
-  if(!s) {
-    return 0
-  }
-  let str = ''
-  let n = 0
-
-  for (ch of s) {
-    const index = str.indexOf(ch)
-    if (index === -1) {
-      str += ch;
-      n = Math.max(n, str.length)
-    } else {
-      str = str.slice(index + 1) + ch
-    }
-  }
-
-  return n
+var superEggDrop = function(K, N) {
+  const step = parseInt(N / K)
 };
-
-console.log(lengthOfLongestSubstring("abcabcbb")) // 3
-console.log(lengthOfLongestSubstring("bbbbb"))  // 1
-console.log(lengthOfLongestSubstring("pwwkew")) // 3
-console.log(lengthOfLongestSubstring("aab"))  // 2
-console.log(lengthOfLongestSubstring("dvdf"))  // 3
